94eb6681e5a21e89482869a77b51a5fcd784abdf
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-20 22:57:29 +0000

    Expose base16 themes

--- src/style.ts
+++ src/style.ts
@@ -14,14 +14,27 @@ const styleDir = join(
 export async function getStyleCss(name: string): Promise<string> {
 	const available = await getStyleNames();
 	if (!available.includes(name)) {
+		console.info(`available styles: ${available.join(", ")}`);
 		throw new Error(`invalid style: ${name}`);
+	}
+	if (name.startsWith("base16-")) {
+		return await readFile(
+			join(styleDir, "base16", `${name.slice(7)}.min.css`),
+			"utf-8",
+		);
 	}
 	return await readFile(join(styleDir, `${name}.min.css`), "utf-8");
 }
 
 export async function getStyleNames(): Promise<string[]> {
-	const files = await readdir(styleDir, { withFileTypes: false });
-	return files
-		.filter((name) => name.endsWith(".min.css"))
-		.map((name) => name.slice(0, -8));
+	const base16themes = await readdir(join(styleDir, "base16"), {
+		withFileTypes: false,
+	});
+	const themes = await readdir(styleDir, { withFileTypes: false });
+	return [
+		...base16themes
+			.filter((name) => name.endsWith(".min.css"))
+			.map((name) => `base16-${name}`),
+		...themes.filter((name) => name.endsWith(".min.css")),
+	].map((name) => name.slice(0, -8));
 }
