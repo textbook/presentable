4dac92382ad300a47b04ed3e81255b53f99a7012
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-21 10:17:03 +0000

    Fix format issue

--- src/browser.ts
+++ src/browser.ts
@@ -2,10 +2,7 @@ import { Browser, launch } from "puppeteer";
 
 export async function createBrowser(): Promise<Browser> {
 	return await launch({
-		args: [
-			"--disable-setuid-sandbox",
-			"--no-sandbox",
-		],
+		args: ["--disable-setuid-sandbox", "--no-sandbox"],
 		defaultViewport: {
 			deviceScaleFactor: 4,
 			height: 768,
