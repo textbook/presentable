092bd1d62ae9ed21aa15aa21e7ed0e88ce08ed43
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-21 10:16:24 +0000

    Disable Chrome sandbox

--- src/browser.ts
+++ src/browser.ts
@@ -2,10 +2,15 @@ import { Browser, launch } from "puppeteer";
 
 export async function createBrowser(): Promise<Browser> {
 	return await launch({
+		args: [
+			"--disable-setuid-sandbox",
+			"--no-sandbox",
+		],
 		defaultViewport: {
 			deviceScaleFactor: 4,
 			height: 768,
 			width: 1_024,
 		},
+		headless: true,
 	});
 }
